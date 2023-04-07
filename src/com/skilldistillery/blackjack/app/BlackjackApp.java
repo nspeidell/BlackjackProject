6d9f54a33ecc0194719408d170b3b58570cab76f
Nick Speidell <nspeidell@gmail.com>
2023-04-07 16:40:04 -0600

    j

---
+++ src/com/skilldistillery/blackjack/app/BlackjackApp.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.blackjack.app;
+
+public class BlackjackApp {
+//everything happens here
+	public static void main(String[] args) {
+		BlackjackApp bja = new BlackjackApp();
+		bja.launch();
+
+	}
+
+	public void launch() {
+		//comments should be methods
+		//a dealer will deal cards
+		
+		
+		
+		//somewhere down the road determine a winner for the game
+		
+	}
+}
